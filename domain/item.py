eecc95bdd4997be2da41a617a7c32dbdac22405c
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2025-05-14 23:49:38 -0300

    feat: adicionar finalização de venda
    Co-authored-by: Arthur <Arthur0006@users.noreply.github.com>

--- domain/item.py
+++ domain/item.py
@@ -1,4 +1,14 @@
 class Item:
-    def __init__(self, ean_produto: str, quantidade: int):
+    def __init__(
+        self,
+        ean_produto: str,
+        descricao: str,
+        preco: float,
+        quantidade: int,
+        unidade: str,
+    ):
         self.ean_produto = ean_produto
+        self.descricao = descricao
+        self.preco = preco
         self.quantidade = quantidade
+        self.unidade = unidade
