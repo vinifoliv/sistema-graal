7648671d966a40716757bbe64ab85a703208aced
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2025-05-29 21:15:30 -0300

    refactor: remover variáveis de ambiente

--- src/database/database.py
+++ src/database/database.py
@@ -1,21 +1,13 @@
 import mysql.connector
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-config = {
-    "user": os.getenv("DATABASE_USER"),
-    "password": os.getenv("DATABASE_PASSWORD"),
-    "host": os.getenv("DATABASE_HOST"),
-    "database": os.getenv("DATABASE_DB"),
-    "raise_on_warnings": True,
-}
-
 
 class Database:
     def __init__(self):
-        self._cnx = mysql.connector.connect(**config)
+        self._cnx = mysql.connector.connect(
+            user="root",
+            password="9572",
+            host="localhost",
+            database="sistema_graal"
+        )
         self._cursor = self._cnx.cursor(buffered=True)
 
     def execute(self, query: str, params=()):
