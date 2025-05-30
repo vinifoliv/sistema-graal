d5df6d6d573afd7bf21515c1d67effcd3663ba17
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2025-05-30 19:28:34 -0300

    refactor: refazer processo de conexão com o banco

--- src/database/database.py
+++ src/database/database.py
@@ -1,23 +1,49 @@
 import mysql.connector
 
+
 class Database:
     def __init__(self):
-        self._cnx = mysql.connector.connect(
-            user="root",
-            password="9572",
-            host="localhost",
-            database="sistema_graal"
+        self._conexao = None
+        self._cursor = None
+
+    def conectar(self):
+        if self._conexao:
+            raise ValueError("Já existe uma conexão com o banco!")
+
+        self._conexao = mysql.connector.connect(
+            user="root", password="9572", host="localhost", database="sistema_graal"
         )
-        self._cursor = self._cnx.cursor(buffered=True)
+        self._cursor = self._conexao.cursor(buffered=True)
+
+    def fechar_conexao(self):
+        if not self._conexao:
+            raise ValueError("Conexão com banco não encontrada!")
+
+        self._conexao.close()
+
+        self._conexao = None
+        self._cursor = None
 
     def execute(self, query: str, params=()):
+        if not self._conexao or not self._cursor:
+            raise ValueError("Nenhuma conexão encontrada!")
+
         self._cursor.execute(query, params)
 
     def fetchone(self):
+        if not self._conexao or not self._cursor:
+            raise ValueError("Nenhuma conexão encontrada!")
+
         return self._cursor.fetchone()
 
     def fetchall(self):
+        if not self._conexao or not self._cursor:
+            raise ValueError("Nenhuma conexão encontrada!")
+
         return self._cursor.fetchall()
 
     def commit(self):
-        self._cnx.commit()  
+        if not self._conexao:
+            raise ValueError("Nenhuma conexão encontrada!")
+
+        self._conexao.commit()
